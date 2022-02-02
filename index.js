dc52ee4ed76320db77b2772f5dbceaaa2718ee23
Rishabh Singh <rishabhsingh2305@gmail.com>
2022-02-02 21:23:49 +0530

    .

--- index.js
+++ index.js
@@ -4,16 +4,7 @@ var port = process.env.PORT || 3000;
 app.listen(port, () => {
 	console.log('listing at port : ', port);
 });
-console.log(__dirname);
 app.use(express.static('public'));
-app.get('/project', (req, res) => {
-	console.log(__dirname);
-	res.sendFile(__dirname + '/public/project/project.html');
-});
-app.get('/contact', (req, res) => {
-	console.log(__dirname);
-	res.sendFile(__dirname + '/public/contacts/contacts.html');
-});
 
 app.get('/panda', (req, res) => {
 	res.sendFile(__dirname + '/assets/panda.png');
