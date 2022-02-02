48740249a845f0d61bfa1f7b3e075c65dcec8a2a
Rishabh Singh <rishabhsingh2305@gmail.com>
2022-02-02 20:35:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+var port = 3000;
+app.listen(port, () => {
+	console.log('listing at port : ', port);
+});
+console.log(__dirname);
+app.use(express.static('public'));
+
+app.get('/home', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
+});
+app.get('/project', (req, res) => {
+	console.log(__dirname);
+	res.sendFile(__dirname + '/public/project/project.html');
+});
+app.get('/contact', (req, res) => {
+	console.log(__dirname);
+	res.sendFile(__dirname + '/public/contacts/contacts.html');
+});
+
+app.get('/panda', (req, res) => {
+	res.sendFile(__dirname + '/assets/panda.png');
+});
